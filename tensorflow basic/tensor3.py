24f5c027d1ee5e2499ea8ad8be70c3fa03d68e0c
wapiti08 <tzrzhuoran@163.com>
2018-07-30 14:40:45 +0800

    Add files via upload

---
+++ tensorflow basic/tensor3.py
@@ -0,0 +1,19 @@
+import tensorflow as tf
+
+#Create a graph
+g=tf.Graph()
+
+#Establish the gragh as the "default" graph
+with g.as_default():
+    #Assemble a graph consisting of the following three operations:
+    # *two tf.constant operations to create the operands
+    # *One tf.add operation to add the two operands
+    x=tf.constant(8,name='x_const')
+    y=tf.constant(5,name='y_const')
+    sum=tf.add(x,y,name="x_y_sum")
+
+    #Now create a session
+    #The seesion will run the default graph
+    with tf.Session() as sess:
+        print(sum.eval())
+        print(sess.run(sum))
