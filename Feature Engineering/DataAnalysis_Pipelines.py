f6183b5b7d005bd7db5f691350bf86719ff010d5
wapiti08 <tzrzhuoran@163.com>
2019-08-31 11:45:26 +0100

    Update DataAnalysis_Pipelines.py

--- Feature Engineering/DataAnalysis_Pipelines.py
+++ Feature Engineering/DataAnalysis_Pipelines.py
@@ -1,56 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# ## Machine Learning Model Building Pipeline: Data Analysis
-# 
-# In the following videos, we will take you through a practical example of each one of the steps in the Machine Learning model building pipeline that we described in the previous lectures. There will be a notebook for each one of the Machine Learning Pipeline steps:
-# 
-# 1. Data Analysis
-# 2. Feature Engineering
-# 3. Feature Selection
-# 4. Model Building
-# 
-# **This is the notebook for step 1: Data Analysis**
-# 
-# We will use the house price dataset available on [Kaggle.com](https://www.kaggle.com/c/house-prices-advanced-regression-techniques/data). See below for more details.
-# 
-# ===================================================================================================
-# 
-# ## Predicting Sale Price of Houses
-# 
-# The aim of the project is to build a machine learning model to predict the sale price of homes based on different explanatory variables describing aspects of residential houses. 
-# 
-# ### Why is this important? 
-# 
-# Predicting house prices is useful to identify fruitful investments, or to determine whether the price advertised for a house is over or underestimated, before making a buying judgment.
-# 
-# ### What is the objective of the machine learning model?
-# 
-# We aim to minimise the difference between the real price, and the estimated price by our model. We will evaluate model performance using the mean squared error (mse) and the root squared of the mean squared error (rmse).
-# 
-# ### How do I download the dataset?
-# 
-# To download the House Price dataset go this website:
-# https://www.kaggle.com/c/house-prices-advanced-regression-techniques/data
-# 
-# Scroll down to the bottom of the page, and click on the link 'train.csv', and then click the 'download' blue button towards the right of the screen, to download the dataset. Rename the file as 'houseprice.csv' and save it to a directory of your choice.
-# 
-# **Note the following:**
-# -  You need to be logged in to Kaggle in order to download the datasets.
-# -  You need to accept the terms and conditions of the competition to download the dataset
-# -  If you save the file to the same directory where you saved this jupyter notebook, then you can run the code as it is written here.
-# 
-# ====================================================================================================
-
-# ## House Prices dataset: Data Analysis
-# 
-# In the following cells, we will analyse the variables of the House Price Dataset from Kaggle. I will take you through the different aspects of the analysis that we will make over the variables, and introduce you to the meaning of each of the variables as well. If you want to know more about this dataset, visit [Kaggle.com](https://www.kaggle.com/c/house-prices-advanced-regression-techniques/data).
-# 
-# Let's go ahead and load the dataset.
-
-# In[3]:
-
-
 # to handle datasets
 import pandas as pd
 import numpy as np
@@ -62,8 +12,6 @@ get_ipython().run_line_magic('matplotlib', 'inline')
 # to display all the columns of the dataframe in the notebook
 pd.pandas.set_option('display.max_columns', None)
 
-
-# In[4]:
 
 
 # load dataset
